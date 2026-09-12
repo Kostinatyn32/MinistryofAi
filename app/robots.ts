fbdf86e548bfb9f607e10ec27d352f8da99897d3
Kostiantyn Tripailo <konstantintripaylo@gmail.com>
2026-09-12 19:25:20 +0200

    Align sitemap with www canonical domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,2 +1,2 @@
 import type {MetadataRoute} from 'next';
-export default function robots():MetadataRoute.Robots{return {rules:process.env.SITE_LIVE==='true'?{userAgent:'*',allow:'/',disallow:['/api/','/start','/dyakuyemo','/oplata-uspishna']}:{userAgent:'*',disallow:'/'},sitemap:'https://ministrysale.org/sitemap.xml'}}
+export default function robots():MetadataRoute.Robots{return {rules:process.env.SITE_LIVE==='true'?{userAgent:'*',allow:'/',disallow:['/api/','/start','/dyakuyemo','/oplata-uspishna']}:{userAgent:'*',disallow:'/'},sitemap:'https://www.ministrysale.org/sitemap.xml'}}
